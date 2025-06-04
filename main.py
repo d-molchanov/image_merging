9a13bcec2d34628eeb6f47edd20e60f3bb74a8aa
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2025-06-05 01:27:45 +0300

    Several imports from PySide6.QtCore have been added.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
-from PySide6.QtCore import QUrl
+from PySide6.QtCore import QUrl, Signal, QObject, Slot, Property
+
+from backend import Backend
 
 
 if __name__ == '__main__':
+    backend = Backend()
+
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
+    engine.rootContext().setContextProperty("backend", backend)
     # engine.load(QUrl("main.qml"))
     engine.load(QUrl("test.qml"))
     if not engine.rootObjects():
